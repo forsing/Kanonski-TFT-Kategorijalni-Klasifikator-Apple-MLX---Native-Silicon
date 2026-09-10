517f38e0648b7fc6d4042edd6649e47b1d5f0a0f
ForsIng <forsing@gmail.com>
2026-09-11 01:59:08 +0200

    Update CSV path in treniraj_v2 function

--- mamba2TFT_MLX_v4b.py
+++ mamba2TFT_MLX_v4b.py
@@ -117,7 +117,7 @@ def loss_fn(model, x, y):
 
 # --- TRENING PETLJA ---
 def treniraj_v2():
-    csv_putanja = "/Users/4c/Desktop/GHQ/data/loto7_4682_k72_loto_2963.csv"
+    csv_putanja = "/data/loto7_4682_k72_loto_2963.csv"
     X, Y = ucitaj_mlx_podatke(csv_putanja, prozor=100)
     
     model = MLXCanonicalTFT()
